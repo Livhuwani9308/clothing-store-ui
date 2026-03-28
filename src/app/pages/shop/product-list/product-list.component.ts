07e691f1aeeb9a3b4882c2474eee309654f92f9f
LivhuwaniKM <lkmasindi1208@gmail.com>
2026-03-28 11:59:33 +0200

    remove product component | product list form design

--- src/app/pages/shop/product-list/product-list.component.ts
+++ src/app/pages/shop/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PRODUCTS } from '../../../data/products.data';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
@@ -7,5 +9,43 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.scss'
 })
 export class ProductListComponent {
+  products: any[] = [];
+  filteredProducts: any[] = [];
 
+  categoryTitle = '';
+
+  maxPrice = 5000;
+  selectedSizes: string[] = [];
+  selectedColors: string[] = [];
+
+  sizes = ['XS', 'S', 'M', 'L', 'XL'];
+  colors = ['Black', 'White', 'Beige', 'Navy', 'Grey'];
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    const category = this.route.snapshot.paramMap.get('category');
+    this.categoryTitle = category?.toUpperCase() || '';
+
+    this.products = PRODUCTS.filter(p => p.category === category);
+    this.filteredProducts = [...this.products];
+  }
+
+  applyFilters() {
+    this.filteredProducts = this.products.filter(product =>
+      product.price <= this.maxPrice
+    );
+  }
+
+  toggleSize(size: string) {
+    this.selectedSizes.includes(size)
+      ? this.selectedSizes = this.selectedSizes.filter(s => s !== size)
+      : this.selectedSizes.push(size);
+  }
+
+  toggleColor(color: string) {
+    this.selectedColors.includes(color)
+      ? this.selectedColors = this.selectedColors.filter(c => c !== color)
+      : this.selectedColors.push(color);
+  }
 }
